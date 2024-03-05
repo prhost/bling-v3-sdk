401c2e1c31be5e44d6d1211aa1d3739e704f5835
Lucas Nunes Pinto Pinheiro <lucasnpinheiro@gmail.com>
2024-03-05 13:41:36 -0300

    Add new methods for Estoques and ProdutosCategorias

---
+++ src/Repositories/Estoques.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Prhost\Bling\Repositories;
+
+use Prhost\Bling\Classes\Collection;
+use Prhost\Bling\Repositories\Traits\CrudRepository;
+
+class Estoques extends BaseRepository
+{
+    use CrudRepository;
+
+    protected $uri = 'estoques';
+
+    protected $queryParam = 'idsProdutos';
+
+}
